38c0fa16827f7dd112c50ff52ef4225c31c05840
betuliskender <34860973+betuliskender@users.noreply.github.com>
2021-09-23 20:04:37 +0200

    Øvelser uge 38

---
+++ Uge38/TaskOne/Main.java
@@ -0,0 +1,12 @@
+class Main{
+
+
+public static void main(String [] args) {
+person("Betül",27);
+  }
+
+public static void person(String name, int age){
+  System.out.println("My name is "+name+", I am "+age+" years old");
+
+  }
+}
